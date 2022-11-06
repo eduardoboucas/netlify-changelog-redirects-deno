9ea8633af7c4fd789a262b401e92292859e89620
Eduardo Bouças <mail@eduardoboucas.com>
2022-11-06 23:23:32 +0000

    feat: add default base URL

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,8 @@ import { getRedirects } from "./mod.ts";
 const flags = parse(Deno.args, {
   string: ["baseURL", "changelog", "target"],
 });
+const baseURL = (flags.baseURL as string) ?? Deno.env.get("URL");
 const changelog = await Deno.readTextFile(flags.changelog as string);
-const redirects = getRedirects(changelog, flags.baseURL as string);
+const redirects = getRedirects(changelog, baseURL);
 
 await Deno.writeTextFile(flags.target as string, redirects);
